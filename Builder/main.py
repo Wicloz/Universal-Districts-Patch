90135c3c7ff2b6bd2012b8df0df92452f06f3ae5
Wilco de Boer <deboer.wilco@gmail.com>
2019-05-26 16:39:05 +0200

    Move helper functions to separate file

--- Builder/main.py
+++ Builder/main.py
@@ -1,12 +1,10 @@
-import shlex
-import tempfile
-import zipfile
 import glob
 import os
 import shutil
 import requests
 from distutils.dir_util import copy_tree
 from bs4 import BeautifulSoup
+from tools import StellarisDict, parse_file, write_data, extract_mod
 
 ########
 # VARS #
@@ -54,134 +52,6 @@ def copytree(src, dst, symlinks=False, ignore=None):
         else:
             if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
                 shutil.copy2(s, d)
-
-
-class StellarisDict(dict):
-    def ensure(self, data):
-        for key, values in data.items():
-            if key not in self:
-                self[key] = []
-            for value in values:
-                if type(value) is str:
-                    self[key].append(value)
-                else:
-                    self[key].append(StellarisDict().ensure(value))
-        return self
-
-    def has_single(self, key):
-        return key in self and len(self[key]) == 1
-
-    def get_single(self, key):
-        if key in self:
-            assert len(self[key]) == 1
-            return self[key][0]
-        else:
-            return StellarisDict()
-
-    def get_list(self, key):
-        if key not in self:
-            return []
-        else:
-            return self[key]
-
-    def copy_without(self, key):
-        copy = StellarisDict(self.copy())
-        if key in copy:
-            del copy[key]
-        return copy
-
-    def get_values(self):
-        for values in self.values():
-            for value in values:
-                if isinstance(value, StellarisDict):
-                    yield from value.get_values()
-                else:
-                    yield value
-
-
-def parse_file(path):
-    with open(path, 'r') as file:
-        text = ''
-        for line in file.readlines():
-            line = line.split('#', 1)[0]
-            for sign in ['=', '>', '<', '{', '}']:
-                line = line.replace(sign, ' ' + sign + ' ')
-            line = line.replace('<  =', '<=').replace('>  =', '>=')
-            text += line + '\n'
-        tokens = ['{'] + [item for item in shlex.split(text) if len(item.strip()) > 0] + ['}']
-        tokens = list(reversed(tokens))
-        return parse_object(tokens, {})
-
-
-def parse_object(tokens, variables):
-    token = tokens.pop()
-    if token is not '{':
-        return token
-
-    data = StellarisDict()
-    while True:
-        key = tokens.pop()
-        if key.upper() in ['IF', 'AND', 'OR', 'NOT', 'NOR', 'NAND']:
-            key = key.upper()
-        if key == '}':
-            return data
-        sign = tokens.pop()
-        if sign not in ['=', '>', '<', '>=', '<=']:
-            tokens.append(sign)
-            value = None
-        else:
-            value = parse_object(tokens, variables.copy())
-            if type(value) is str and not key.startswith('@'):
-                if value in variables:
-                    value = variables[value]
-                value = sign + ' ' + value
-        if (key == 'OR' or key == 'AND') and len(value.keys()) == 1 and len(value[list(value.keys())[0]]) <= 1:
-            key = list(value.keys())[0]
-            value = value[key][0] if len(value[key]) == 1 else None
-        if key.startswith('@'):
-            variables[key] = value
-        else:
-            if key not in data.keys():
-                data[key] = list()
-            if value is not None:
-                data[key].append(value)
-
-
-def write_data(data, path):
-    with open(path, 'w') as file:
-        write_object(data, file, 0)
-
-
-def write_object(data, file, level):
-    if type(data) is str:
-        file.write(' ' + data + '\n')
-        return
-
-    if level > 0:
-        file.write(' = {\n')
-    for key, values in data.items():
-        if len(values) == 0:
-            write_tabs(level, file)
-            file.write(key + '\n')
-        for value in values:
-            write_tabs(level, file)
-            file.write(key)
-            write_object(value, file, level + 1)
-    if level > 0:
-        write_tabs(level - 1, file)
-        file.write('}\n')
-
-
-def write_tabs(count, file):
-    for _ in range(count):
-        file.write('\t')
-
-
-def extract_mod(mod_id):
-    folder = tempfile.TemporaryDirectory().name
-    with zipfile.ZipFile(glob.glob(workshop_folder + '/' + mod_id + '/*.zip')[0], 'r') as mod:
-        mod.extractall(folder)
-    return folder
 
 
 def use_mod_flag(mod):
@@ -250,7 +120,7 @@ if __name__ == '__main__':
 
         # load mod data
         try:
-            mod_folder = extract_mod(other_mod[0])
+            mod_folder = extract_mod(workshop_folder, other_mod[0])
         except IndexError:
             print('Skipping mod with id: ' + other_mod[0])
             continue
